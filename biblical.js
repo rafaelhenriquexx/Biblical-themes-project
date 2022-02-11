56fdd86114c989890a2aac658e6be2c807fdb32c
Rafael Henrique <rafaelpharrell@hotmail.com>
2022-02-11 19:00:53 -0300

    added new README

--- biblical.js
+++ biblical.js
@@ -4,7 +4,9 @@ changeText = () => {
         const paragraphValue = document.getElementById('paragraph-value')
         //const segundoVersiculo = document.getElementById('segundo-versiculo')
         
-
+        if(select.value === 'Como encontrar felicidade?'){
+            paragraphValue.innerHTML = "<strong>Lucas 11:28</strong> - Felizes os que ouvem a palavra de Deus e a põem em prática!"
+        }
         if(select.value === 'Como lidar com a Ansiedade?'){
             paragraphValue.innerHTML = 
             "<strong>Isaías 41:10</strong> - Não tenha medo, pois estou com você. Não fique ansioso, pois eu sou o seu Deus. Vou fortalecê-lo, sim, vou ajudá-lo. Vou segurá-lo firmemente com a minha mão direita de justiça."
@@ -16,9 +18,7 @@ changeText = () => {
         if(select.value === 'O que a Bíblia promete para o Futuro?'){
             paragraphValue.innerHTML = "<strong>Apocalipse 21:4</strong> - Ele enxugará dos seus olhos toda lágrima, e não haverá mais tristeza, nem choro, nem dor. As coisas anteriores já passaram."
         }
-        if(select.value === 'Como encontrar felicidade?'){
-            paragraphValue.innerHTML = "<strong>Lucas 11:28</strong> - Felizes os que ouvem a palavra de Deus e a põem em prática!"
-        }
+        
             
          
  
